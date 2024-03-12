738a7dc5807d8860441c1125077edcdcb0754858
Bamberek <kabaczek99@gmail.com>
2024-03-12 23:36:00 +0100

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Background from "./components/Background/Background";
 import InputShortener from "./components/InputShortener/InputShortener";
+import LinkResult from "./components/LinkResult/LinkResult";
 import "./styles/globals.css";
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div className="container">
-      <InputShortener />
+      <InputShortener setInputValue={setInputValue} />
+      <Background />
+      <LinkResult inputValue={inputValue} />
     </div>
   );
 }
