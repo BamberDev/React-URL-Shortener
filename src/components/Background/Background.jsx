5e5332a94ed70bce387a1402a75d9aad9c833187
Bamberek <kabaczek99@gmail.com>
2024-03-12 23:34:30 +0100

    added background

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,32 @@
+import styles from "./Background.module.scss";
+
+function Background() {
+  return (
+    <div>
+      <ul className={styles.background}>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Background;
