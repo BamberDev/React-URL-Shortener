768a76e5128ad97d6e0e6f718d7b54c93687a544
Bamberek <kabaczek99@gmail.com>
2024-03-12 02:55:13 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import InputShortener from "./components/InputShortener/InputShortener";
+import "./styles/globals.css";
+function App() {
+  return (
+    <div className="container">
+      <InputShortener />
+    </div>
+  );
+}
+
+export default App;
