412d685b911ab1e8ac4802752aea4c362f09b59a
Bamberek <kabaczek99@gmail.com>
2024-05-28 23:38:15 +0200

    updated background

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,6 +1,13 @@
+import useWindowWidth from "../UseWindowWidth/UseWindowWidth";
 import styles from "./Background.module.scss";
 
 function Background() {
+  const windowWidth = useWindowWidth();
+
+  if (windowWidth < 768) {
+    return null;
+  }
+
   return (
     <div>
       <ul className={styles.background}>
